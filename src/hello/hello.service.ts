066f6bfc06f694561d2939563f161f6b89106075
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2020-11-13 15:27:16 +0330

    Add welcome method to hello service

--- src/hello/hello.service.ts
+++ src/hello/hello.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { PersonDto } from './dto/person.dto';
 
 @Injectable()
-export class HelloService {}
+export class HelloService {
+  async welcome(person: PersonDto): Promise<string> {
+    let msg: string;
+    if (person.year) {
+      const current_year = new Date().getFullYear();
+      console.log(`Welcome ${person.name} - your birthday is ${person.year}`);
+      msg = `Welcome ${person.name} - you are ${
+        current_year - person.year
+      } years old!`;
+    } else {
+      console.log(`Welcome ${person.name} - your birthday is Undefined`);
+      msg = `Welcome ${person.name} - your birthday is Undefined!!!`;
+    }
+    return msg;
+  }
+}
