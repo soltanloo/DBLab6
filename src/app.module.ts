5aa5e577b00cbc43715bed396fcadc5a7d07789c
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2020-11-13 17:58:40 +0330

    Configure typeorm in top module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,20 @@ import { HelloModule } from './hello/hello.module';
 import { BooksModule } from './books/books.module';
 import { GenreModule } from './genre/genre.module';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import UserEntity from './db/entity/user.entity';
+import BookEntity from './db/entity/book.entity';
+import GenreEntity from './db/entity/genre.entity';
 
 @Module({
-  imports: [HelloModule, BooksModule, GenreModule, UserModule],
+  imports: [
+    HelloModule,
+    BooksModule,
+    GenreModule,
+    UserModule,
+    TypeOrmModule.forFeature([UserEntity, BookEntity, GenreEntity]),
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
