e16a05a01c680bd4b1a248a2159582da59b434a5
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2020-11-13 17:58:14 +0330

    Fix books controller

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,26 +1,4 @@
-import BookEntity from '../db/entity/book.entity';
-import CreateBookDto from '../dto/create-book.dto';
-import UserEntity from '../db/entity/user.entity';
-import { createQueryBuilder, getConnection } from 'typeorm';
-import GenreEntity from '../db/entity/genre.entity';
+import { Controller } from '@nestjs/common';
 
-export class BooksService {
-  async insert(bookDetails: CreateBookDto): Promise<BookEntity> {
-    const { name, userID, genreIDs } = bookDetails;
-    const book = new BookEntity();
-    book.name = name;
-    book.user = await UserEntity.findOne(userID);
-    book.genres = [];
-    for (let i = 0; i < genreIDs.length; i++) {
-      const genre = await GenreEntity.findOne(genreIDs[i]);
-      book.genres.push(genre);
-    }
-    await book.save();
-    return book;
-  }
-
-  async getAllBooks(): Promise<BookEntity[]> {
-    // const user: UserEntity = await UserEntity.findOne({where: {id: 2}, relations: ['books']});
-    return BookEntity.find();
-  }
-}
+@Controller('books')
+export class BooksController {}
