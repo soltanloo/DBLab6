bbdddf413711f2ff7048349b7b38d23832b06c69
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2020-11-13 16:36:56 +0330

    Add swagger configs to dto

--- src/hello/dto/person.dto.ts
+++ src/hello/dto/person.dto.ts
@@ -1,11 +1,23 @@
 import { Length, IsOptional, Min, IsNumber } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class PersonDto {
   @Length(3, 10)
+  @ApiProperty({
+    description: 'Enter Your Name > ',
+    minLength: 3,
+    default: 'Hossein',
+    maxLength: 10,
+  })
   name: string;
 
   @IsNumber()
   @IsOptional()
   @Min(1960)
+  @ApiPropertyOptional({
+    description: 'Optional',
+    default: 1998,
+    minimum: 1960,
+  })
   year: number;
 }
