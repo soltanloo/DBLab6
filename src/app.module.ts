637b6324b5cd1218c09084b6c1b897ba91b094ea
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2020-11-13 17:42:09 +0330

    Add books, user, and genre modules to top module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloModule } from './hello/hello.module';
+import { BooksModule } from './books/books.module';
+import { GenreModule } from './genre/genre.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [HelloModule],
+  imports: [HelloModule, BooksModule, GenreModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
