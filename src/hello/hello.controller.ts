549550eb8cd696caf93e6cf04a77aa8aa514b2cd
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2020-11-13 16:34:57 +0330

    Add swagger configs to controller

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Post, Body, Get, Header, Query } from '@nestjs/common';
 import { HelloService } from './hello.service';
 import { PersonDto } from './dto/person.dto';
+import { ApiResponse, ApiQuery } from '@nestjs/swagger';
 
 @Controller('hello')
 export class HelloController {
   constructor(private readonly helloService: HelloService) {}
 
+  @ApiResponse({ status: 200, description: 'Say Hello!!!' })
   @Post('welcome')
   @Header('Content-Type', 'application/json')
   async sayWelcome(@Body() personDto: PersonDto): Promise<{ data: string }> {
@@ -13,6 +15,18 @@ export class HelloController {
     return { data: msg };
   }
 
+  @ApiResponse({ status: 200 })
+  @ApiQuery({
+    name: 'name',
+    required: true,
+    type: String,
+  })
+  @ApiQuery({
+    name: 'year',
+    required: false,
+    type: Number,
+    description: 'you can ignore this',
+  })
   @Get('welcome')
   async sayWelcome2(
     @Query('name') name,
