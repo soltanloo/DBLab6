d7670da2320ab445d1c49393d8c6cbb73a3aa9d3
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2020-11-13 15:31:23 +0330

    Add welcome post method to hello service

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Header } from '@nestjs/common';
+import { HelloService } from './hello.service';
+import { PersonDto } from './dto/person.dto';
 
 @Controller('hello')
-export class HelloController {}
+export class HelloController {
+  constructor(private readonly helloService: HelloService) {}
+
+  @Post('welcome')
+  @Header('Content-Type', 'application/json')
+  async sayWelcome(@Body() personDto: PersonDto): Promise<{ data: string }> {
+    const msg = await this.helloService.welcome(personDto);
+    return { data: msg };
+  }
+}
