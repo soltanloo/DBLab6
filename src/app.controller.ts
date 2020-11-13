614215e30a1d71d19144cdbf27e4d04564287803
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2020-11-13 17:02:32 +0330

    Clean up the app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
 
-@Controller('/hello')
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get('/hi')
-  getHello(): string {
-    return this.appService.getHello();
-  }
-
-  @Get('/bye')
-  getBye(): string {
-    return this.appService.getHello();
-  }
-}
+@Controller()
+export class AppController {}
