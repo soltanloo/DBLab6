6284df96033f67bc5bf19561112a0792534c4db8
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2020-11-13 17:18:03 +0330

    Add user entity

---
+++ src/db/user.entity.ts
@@ -0,0 +1,17 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BaseEntity,
+  OneToMany,
+} from 'typeorm';
+import BookEntity from './book.entity';
+
+@Entity()
+export default class UserEntity extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 500 })
+  name: string;
+}
