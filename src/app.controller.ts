be72fb0bde921fd8c03e76f5e95eeb16cf0eb104
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2020-11-16 20:04:36 +0330

    Finish app controller implementation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,20 +9,27 @@ import {
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import LoginDto from './dto/login.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { UserService } from './user/user.service';
 
 @Controller()
 export class AppController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('auth/login')
-  async login(@Request() req) {
+  async login(@Request() req, @Body() body: LoginDto) {
     return this.authService.login(req.user);
   }
 
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
-    return req.user;
+    return this.userService.getUser(req.user.username);
   }
 }
