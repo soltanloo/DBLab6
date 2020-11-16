f72b07f1a1df929b4d9b550b0eb1f70cf52abfed
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2020-11-16 20:02:26 +0330

    Clean up user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,43 +1,14 @@
-import {
-  Body,
-  Controller,
-  Get,
-  ParseIntPipe,
-  Post,
-  Put,
-  Query,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import CreateUserDto from '../dto/create-user.dto';
-import { ApiBearerAuth, ApiQuery, ApiResponse } from '@nestjs/swagger';
-import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { ApiResponse } from '@nestjs/swagger';
 
-@Controller('users')
+@Controller('user')
 export class UserController {
   constructor(private readonly usersService: UserService) {}
   @ApiResponse({ status: 200, description: 'New user created successfully' })
-  @Post('post')
+  @Post('')
   postUser(@Body() user: CreateUserDto) {
     return this.usersService.insert(user);
-  }
-  @ApiBearerAuth()
-  @UseGuards(JwtAuthGuard)
-  @ApiResponse({ status: 200, description: 'Get all users successfully' })
-  @Get()
-  getAll() {
-    return this.usersService.getAllUsers();
-  }
-  @ApiBearerAuth()
-  @UseGuards(JwtAuthGuard)
-  @ApiResponse({ status: 200, description: 'Get books of a user successfully' })
-  @ApiQuery({
-    name: 'userID',
-    required: true,
-    type: Number,
-  })
-  @Get('books')
-  getBooks(@Query('userID') userID: number) {
-    return this.usersService.getBooksOfUser(userID);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import UserEntity from '../db/entity/user.entity';
 import CreateUserDto from '../dto/create-user.dto';
-import BookEntity from '../db/entity/book.entity';
-import { getConnection } from 'typeorm';
-import { options } from 'tsconfig-paths/lib/options';
 
 @Injectable()
 export class UserService {
@@ -17,18 +14,9 @@ export class UserService {
     return userEntity;
   }
   async getUser(username: string): Promise<UserEntity> {
-    const user = await UserEntity.findOne({ where: { username } });
-    return user;
-  }
-  async getAllUsers(): Promise<UserEntity[]> {
-    return await UserEntity.find();
-  }
-  async getBooksOfUser(userID: number): Promise<BookEntity[]> {
-    console.log(typeof userID);
-    const user: UserEntity = await UserEntity.findOne({
-      where: { id: userID },
-      relations: ['books'],
+    return await UserEntity.findOne({
+      where: { username },
+      relations: ['todos'],
     });
-    return user.books;
   }
 }
