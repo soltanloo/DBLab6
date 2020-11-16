f6a84f47e8ad291d7cf9fc739b258d6dbd5c0259
Hossein Soltanloo <hossein.soltanloo@yahoo.com>
2020-11-16 14:41:04 +0330

    Delete genre module

--- src/genre/genre.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import GenreService from './genre.service';
-import GenreController from './genre.controller';
-
-@Module({
-  providers: [GenreService],
-  controllers: [GenreController],
-})
-export class GenreModule {}
